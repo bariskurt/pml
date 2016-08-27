493e6727f6d7eefc55145b31062b0395815dd396
Baris Kurt <bariskurt@gmail.com>
2016-08-27 19:28:17 +0300

    a small python package added, for reading and writing txt data files in pml file format

---
+++ python/pml/pml.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def loadTxt(filename):
+    X = np.loadtxt(filename)
+    dim = int(X[0]);
+    size = []
+    for i in range(dim):
+        size.append(int(X[i+1]));
+    X = np.reshape(X[dim+1:], size, order='F')
+    return X;
+
+def saveTxt(filename, X, format = '%.6f'):
+    with open(filename, 'w') as f:
+        dim = len(X.shape)
+        f.write('%d\n' % dim)
+        for i in range(dim):
+            f.write('%d\n' % X.shape[i])
+        temp = X.reshape(np.product(X.shape), order='F')
+        np.savetxt(f, temp, fmt = format)
---
+++ python/pml/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pml', 
+    version='0.1', 
+    author='Baris Kurt', 
+    author_email='bariskurt@gmail.com', 
+    url='www.bariskurt.com'
+)
